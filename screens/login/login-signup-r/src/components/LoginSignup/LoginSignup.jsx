c063ab9b797cb429ee6615413f8b867aff953f00
Daiwik R <daiwik.bhasker04@gmail.com>
2023-11-13 15:02:56 +0530

    Made the login page functional

--- screens/login/login-signup-r/src/components/LoginSignup/LoginSignup.jsx
+++ screens/login/login-signup-r/src/components/LoginSignup/LoginSignup.jsx
@@ -5,18 +5,20 @@ import email from '../assets/email.png';
 import password from '../assets/password.png';
 import user from '../assets/user.png';
 
+
 export default function LoginSignup() {
+    const[action, setAction] = React.useState("Sign-Up");
   return (
     <div className="container">
     <div className="header">
-    <div className="text">Sign-Up</div>
+    <div className="text">{action}</div>
     <div className="underline"></div>  
     </div>
     <div className="inputs">
-        <div className="input">
+        {action==="Login"?<div></div>:<div className="input">
         <img src={user} alt="user" />
         <input type="text" placeholder='Username'/>
-        </div>
+        </div>}
         <div className="input">
         <img src={email} alt="email-icon" />
         <input type="email" placeholder='Email Id'/>
@@ -26,10 +28,10 @@ export default function LoginSignup() {
         <input type="password" placeholder='Password'/>
         </div>
     </div>
-    <div className="forgot-password">Forgot Password?<span>Click Here!</span></div>
+    {action==="Sign-Up"?<div></div>:<div className="forgot-password">Forgot Password?<span>Click Here!</span></div>}
     <div className="submit-container">
-        <div className="submit">Sign-Up</div>
-        <div className="submit">Login</div>
+        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign-Up")}}>Sign-Up</div>
+        <div className={action==="Sign-Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
         </div>
     </div>
   )
