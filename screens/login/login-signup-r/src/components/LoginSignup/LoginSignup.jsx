496d549ad73e398aee55f6540067cb36c8dd5e8d
Daiwik R <daiwik.bhasker04@gmail.com>
2023-11-13 14:35:25 +0530

    added css styling to login/signup page

--- screens/login/login-signup-r/src/components/LoginSignup/LoginSignup.jsx
+++ screens/login/login-signup-r/src/components/LoginSignup/LoginSignup.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import './LoginSignup.css'
 
+import email from '../assets/email.png';
+import password from '../assets/password.png';
+import user from '../assets/user.png';
+
 export default function LoginSignup() {
   return (
-    <div>
-      
+    <div className="container">
+    <div className="header">
+    <div className="text">Sign-Up</div>
+    <div className="underline"></div>  
+    </div>
+    <div className="inputs">
+        <div className="input">
+        <img src={user} alt="user" />
+        <input type="text" placeholder='Username'/>
+        </div>
+        <div className="input">
+        <img src={email} alt="email-icon" />
+        <input type="email" placeholder='Email Id'/>
+        </div>
+        <div className="input">
+        <img src={password} alt="password-icon" />
+        <input type="password" placeholder='Password'/>
+        </div>
+    </div>
+    <div className="forgot-password">Forgot Password?<span>Click Here!</span></div>
+    <div className="submit-container">
+        <div className="submit">Sign-Up</div>
+        <div className="submit">Login</div>
+        </div>
     </div>
   )
 }
